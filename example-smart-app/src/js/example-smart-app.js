531693d43b79bc16801f260cea24022e4692ded6
Ethan Jen <ejen091@gmail.com>
2019-04-20 18:41:30 -0400

    revert

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -60,9 +60,11 @@
               $.when(pt, enc).done(function(pt, enc) {
                 loc = enc.data.location[0].location.display;
                 doc = enc.data.participant[0].individual.display;
-                var element = {id: idn, name: pname, order: dorder, completedDate: completedDateValue, status: statusValue, color: col, doctor: doc, location: loc};
-                diagdata.push(element);
+                console.log(loc);
+                console.log(doc);
               });
+              var element = {id: idn, name: pname, order: dorder, completedDate: completedDateValue, status: statusValue, color: col, doctor: doc, location: loc};
+              diagdata.push(element);
               
               idn = idn + 1;
             }
